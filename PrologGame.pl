1f0f90e51a2838d5fb1a04d93bb41450aeed4fea
Pawel Nowotarski <pawnow6@gmail.com>
2016-11-01 16:53:08 +0100

    First Prolog Game Commit

---
+++ PrologGame.pl
@@ -0,0 +1,67 @@
+ :- dynamic level/1, hero_position/3, alive/1, position/2, equipment/1.
+hero_position(meadow,2,2).
+location(meadow, 2, 2).
+location(cave, 1, 2).
+location(cave_boss, 0, 2).
+
+position(cave_boss, dragon).
+alive(dragon).
+
+north :- go(0, 1).
+south :- go(0, -1).
+east :- go(-1, 0).
+west :- go(1, 0).
+
+go(XDelta, YDelta) :-
+        hero_position(Place, X, Y),
+		X1 is X + XDelta,
+		Y1 is Y + YDelta,
+        location(NewPlace, X1, Y1),
+        retract(hero_position(Place, X, Y)),
+        assert(hero_position(NewPlace, X1, Y1)),
+        show, !.
+
+go(_) :-
+        write('That is nothing interesting there, choose other direction').
+
+show :-
+        hero_position(Place, X, Y),
+        describe(Place),
+		describeActions(Place, X, Y),
+        nl.
+
+start :-
+        show.
+
+describe(meadow) :-
+        write('You wake up in the middle of the meadow with nothing in hands.'), nl,
+        write('You see a cave in the east'), nl.
+		
+describe(cave) :-
+        write('You enter the cave, there is dark inside.'), nl.
+		
+describe(cave_boss) :-
+        write('You see a dragon inside a cave and it kills you.'), nl.
+
+describe(_, _) :-
+	write('').
+	
+describeActions(Place, X, Y) :-
+	nl, write('Possible actions in the current location:'), nl,
+	XWest is X + 1,
+	XEast is X - 1,
+	YNorth is Y + 1,
+	YSouth is Y - 1,
+	describeDirection('You can go West', XWest, Y),
+	describeDirection('You can go East', XEast, Y),
+	describeDirection('You can go North', X, YNorth),
+	describeDirection('You can go South', X, YSouth).
+
+describeDirection(Direction, X, Y) :-
+	location(Place, X, Y),
+	write(Direction),
+	write(' to find '),
+	write(Place), nl.
+	
+describeDirection(_, _, _) :-
+	write('').
